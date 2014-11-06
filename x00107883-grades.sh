22f1c826b505052da7a585a522509345cd0c9fac
X00107883 <X00107883@gmail.com>
2014-11-06 20:52:41 +0000

    BlackBoxTest regression test x00107883

--- x00107883-grades.sh
+++ x00107883-grades.sh
@@ -2,37 +2,37 @@
 echo Test Case 1
 	echo  Inputs:  40 50
 	echo  Expected output: "Fail"
-	AO=$(java grades 40 50)
+	AO=$(java grades_v2 40 50)
 	echo Actual output: $AO
 echo
 echo Test Case 2
 	echo Inputs: -2  50
 	echo Expected output: "Invalid input"
-	AO=$(java grades -2 50)
+	AO=$(java grades_v2 -2 50)
 	echo Actual output: $AO
 echo
 echo Test Case 3
 	echo Inputs: 40 102
 	echo Expected output: "Invalid input"
-	AO=$(java grades 40 102)
+	AO=$(java grades_v2 40 102)
 	echo Actual output: $AO
 echo
 echo Test Case 4
 	echo Inputs: 39 100
 	echo Expected output: "Component Fail"
-	AO=$(java grades 39 100)
+	AO=$(java grades_v2 39 100)
 	echo Actual output: $AO
 echo
 echo Test Case 5
 	echo Inputs: 90 39
 	echo Expected output: "Component Fail"
-	AO=$(java grades 90 39)
+	AO=$(java grades_v2 90 39)
 	echo Actual output: $AO
 echo
 echo Test Case 6
 	echo Inputs: 50 50
 	echo Expected output: "Pass"
-	AO=$(java grades 50 50)
+	AO=$(java grades_v2 50 50)
 	echo Actual output: $AO
 echo
 
